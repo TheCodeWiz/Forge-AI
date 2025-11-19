50b023a6b2d4d863a6c8033fad1fcb5d8a677b66
Manav Bhatt <162689343+TheCodeWiz@users.noreply.github.com>
2025-11-19 19:29:59 +0530

    Update AiModel.jsx

--- configs/AiModel.jsx
+++ configs/AiModel.jsx
@@ -4,8 +4,9 @@ const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
+  model: "gemini-2.5-flash",
   // model: "gemini-2.0-flash",
-  model: "gemini-3-pro",
+  // model: "gemini-3-pro",
 });
 
 const generationConfig = {
